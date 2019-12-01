08c70931e145075f4c3a5e055e6c3268e2ebc390
thanakorn boohuadchai <thanakornboohuadchai@thanakorns-MacBook-Pro.local>
2019-12-01 12:58:39 +0700

    commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
+  test: any;
+  ArrayTest: any[];
+  constructor(public Http:HttpClient) {
+    let datas = this.Http.get('https://api.worldofwarships.asia/wows/encyclopedia/battletypes/?application_id=be2e3527b2ce60fa8239e31e2f367271');
+    datas.subscribe(result=>{
+      this.test = result;
+      console.log(this.test);
+      console.log(JSON.stringify(this.test));
+      console.log('BT : ',this.test.status);
+      console.log('BT : ',this.test.data['CLAN']);
+      this.ArrayTest = this.test.data;
+      console.log('test Array : ',this.ArrayTest);
+      console.log('0');
+      for (let i in this.test.data){
+        console.log(this.test.data[i]);
+        
+      }
+      //console.log(this.ArrayTest['CLAN']);
+    })
+    //console.log(JSON.parse(JSON.stringify(datas)));
+  }
 
 }
